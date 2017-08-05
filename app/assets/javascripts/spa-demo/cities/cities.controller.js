bcc69857cca0ae1f96885e7a50e841162a72440b
alfha101 <ahamard@gmail.com>
2017-08-05 02:48:38 +0200

    added select and update/delete resource instance

--- app/assets/javascripts/spa-demo/cities/cities.controller.js
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -30,7 +30,9 @@
         function handleError(response) {
           console.log(response);
         }
-        function edit(object, index) {
+        function edit(object) {
+          console.log("selected", object);
+          vm.city = object;
         }
 
         function create() {
@@ -45,10 +47,35 @@
         }
 
         function update() {
+          //console.log("update", vm.city)
+          vm.city.$update()
+            .then(function(response){
+              //console.log(response);
+          })
+          .catch(handleError);
         }
+
         function remove() {
+          //console.log("remove", vm.city);
+          vm.city.$delete()
+            .then(function(response){
+              //console.log(response)
+              //remove the element from local array
+              removeElement(vm.cities, vm.city);
+              //vm.cities = City.query();
+              //replace edit area with prototype instance
+              newCity();
+            })
+            .catch(handleError);
         }
+
         function removeElement(elements, element) {
+          for (var i=0; i<elements.length; i++) {
+            if (elements[i].id == element.id) {
+              elements.splice(i,1);
+              break;
+            }
+          }
         }
     }
 })();
